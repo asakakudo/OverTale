34e967cd2f603d1436f2df405270d42c8d1e5bbf
RamaUdiana <ramaputra010906@gmail.com>
2025-11-24 11:40:52 +0800

    main code

--- Overtale.c
+++ Overtale.c
@@ -28,8 +28,8 @@ int main() {
 
     //variables
 
-    int suspicion = 0;
-    char Decision[10]; 
+    int suspicion = 0; 
+    int Decision[4];
     const char storyOp1[] =
         "Deep beneath the surface of the earth, hidden away from the touch of sunlight,\n"
         "there existed a world of darkness unknown to mankind.\n"
@@ -44,12 +44,21 @@ int main() {
         "Until one night, a crack in the earth opened,\n"
         "and the moonlight touched your body for the very first time.\n"
         "This was the moment you've been waiting for.\0";
-
     const char storyOp2[] = 
-        "It crawled out of the abyss and was met with the sight of the surface,\n"
-        "It is beautifull, stunning, nothing like it have ever seen, the moon light shines on it body\n"
-        "the wind of the night soothing it's skin, and slithers through the pine trees, blessing it's ears with such peacefull ambiemce\n\n"
-        "as it admires his soroundings";
+        "you crawled out of the abyss and was met with the sight of the surface,\n"
+        "It is beautifull, stunning, nothing like you've ever seen, the moon light shines on your body,\n"
+        "the wind of the night soothes your skin,\n"
+        "it slithers through the pine trees, blessing your ears with such peacefull ambience.\n\n"
+        "As you admire your soroundings\0"
+        "You heard something\0"
+        "In the bushes nearby there was something making noises"
+        "The creature slowly, reveals itself as it walks towards you,\n"
+        "letting the moonlight reveal it's form, you see that it walks on all fours\n"
+        "it is covered in fluff from head to tail, unlike you.\n\n"
+        "As you were admiring it, it spotted you\0"
+        "Slowly, the creature approaches you with curiosity\0"
+        "The creature doesnt seem to pose any threat to you, but rather has interest in you\0"
+        "You can feel air coming coming out of it snouts as it sniffes you";
 
     //main code
 
@@ -75,14 +84,21 @@ int main() {
     slow_print(storyOp1, 35, 418); //print lanjutan story pertama
     printf("\n\n");
 
-
-
     printf("Press ENTER to continue...");
     getchar(); // waits for ENTER
-    printf("\n\n");
+    printf("\n");
     
     slow_print(storyOp2, 35, 0); //print story opening kedua setelah title 
+    dot_animation();
+    printf("\n\n");
+
+    slow_print(storyOp2, 35, 325); //print story opening kedua (mc mendengar sesuatu)
+    dot_animation();
+    printf("\n\n");
+
+    slow_print(storyOp2, 35, 325); //print story opening kedua (mc mendengar sesuatu)
+    dot_animation();
+    printf("\n\n");
 
     return 0;
-
 }
