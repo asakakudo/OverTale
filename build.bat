1330803e6f5adfaf25b01ab036a62392d72b8df2
AsakaKudo <ptpradnya2@gmail.com>
2025-12-14 20:18:26 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ cd /d "%~dp0"
 echo Compiling Overtale...
 gcc main.c util.c story.c -o overtale
 if %errorlevel% neq 0 (
-    echo [Error] Ada error saat compile!
+    echo [Error] There was an error while compiling!
     pause
 ) else (
     echo [Success] Running the game...
